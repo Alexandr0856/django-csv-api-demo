a9001d889b42b1f23932d98092ad844c8494d498
Olexandr Savchuk <ppos19soo@gmail.com>
2024-07-07 09:28:54 +0300

    update path

--- api/urls.py
+++ api/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from api.views import DynamicDataView, CSVUploadView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('upload-csv/', CSVUploadView.as_view(), name='upload-csv'),
+    path('data/', DynamicDataView.as_view(), name='data-list'),
 ]
